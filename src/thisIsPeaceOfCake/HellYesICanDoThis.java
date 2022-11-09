09ee7db3c604817e51fddddd38e0463d69327b0e
panthersClass <test@tekschool.us>
2022-11-08 21:53:20 -0500

    this is on practice branch

--- src/thisIsPeaceOfCake/HellYesICanDoThis.java
+++ src/thisIsPeaceOfCake/HellYesICanDoThis.java
@@ -5,4 +5,8 @@ public class HellYesICanDoThis {
 	public void iCanLearnGit() {
 		System.out.println("I learn this or i will fast for rest of the night ");
 	}
+	
+	public void thisIsFunWhenItWorks() {
+		System.out.println("i will learn it regardless of issues i am facing");
+	}
 }
